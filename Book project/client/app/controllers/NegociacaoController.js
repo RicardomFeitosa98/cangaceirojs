a0ea5cb4ff4fb95e60a81f715699e749a7c4c313
Ricardo <ricardomfeitosa98@gmail.com>
2023-01-31 21:20:21 -0300

    fix: fixing an error on app.js

--- Book project/client/app/controllers/NegociacaoController.js
+++ Book project/client/app/controllers/NegociacaoController.js
@@ -1,11 +1,11 @@
 class	NegociacaoController	{
 	adiciona(event)	{
 	event.preventDefault();
-//	a	ideia	é	que	$	seja	o	querySelector
-let	$	=	document.querySelector;
-let	inputData	=	$('#data');
-let	inputQuantidade	=	$('#quantidade');
-let	inputValor	=	$('#valor');
+
+    let	$ = document.querySelector.bind(document);
+let	inputData =$('#data');
+let	inputQuantidade	=$('#quantidade');
+let	inputValor =$('#valor');
 console.log(inputData.value);
 console.log(parseInt(inputQuantidade.value));
 console.log(parseFloat(inputValor.value));
