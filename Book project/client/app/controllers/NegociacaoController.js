b437bb310946b297c0aa1cf890b8e02cd4c17b98
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-01 00:03:43 -0300

    feat: adding event to NegociacaoCOntroller that retrives input data

--- Book project/client/app/controllers/NegociacaoController.js
+++ Book project/client/app/controllers/NegociacaoController.js
@@ -1,15 +1,29 @@
-class	NegociacaoController	{
+class NegociacaoController {
 
-    constructor()	{
-        let	$	=	document.querySelector.bind(document);
-        this._inputData	=	$('#data');
-        this._inputQuantidade	=		$('#quantidade');
-        this._inputValor	=	$('#valor');
-	}
-	adiciona(event)	{
-	    event.preventDefault();
-        console.log(this._inputData.value);
-        console.log(parseInt(this._inputQuantidade.value));
-        console.log(parseFloat(this._inputValor.value));
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
     }
 }
