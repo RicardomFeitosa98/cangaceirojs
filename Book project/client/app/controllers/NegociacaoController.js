433da79e173c7b290545eca0f7f784d746ca602e
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-01 13:57:46 -0300

    fix: fixing an script order issue on index.html

--- Book project/client/app/controllers/NegociacaoController.js
+++ Book project/client/app/controllers/NegociacaoController.js
@@ -1,22 +1,28 @@
 class NegociacaoController {
-
-    constructor() {
-
-        let $ = document.querySelector.bind(document);
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
+    constructor()	{
+        let	$	=	document.querySelector.bind(document);
+        this._inputData	= $('#data');
+        this._inputQuantidade =	 $('#quantidade');
         this._inputValor = $('#valor');
-    }
-
+        this._negociacoes = new Negociacoes();
+            }
+   
     adiciona(event) {
 
         event.preventDefault();
+
         let	negociacao	=	new	Negociacao(
         DateConverter.paraData(this._inputData.value),
         parseInt(this._inputQuantidade.value),
         parseFloat(this._inputValor.value)
-            );
+         );
+
+        this._negociacoes.adiciona(negociacao);
+
+        console.log(this._negociacoes.paraArray());
+
         console.log(negociacao.data);
+
         let	diaMesAno=DateConverter.paraTexto(negociacao.data);
         console.log(diaMesAno);
 
