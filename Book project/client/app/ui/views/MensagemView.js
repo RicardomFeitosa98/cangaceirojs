4fdf9503959cba2163fc451df824c56819b0339e
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-02 08:14:04 -0300

    feat: creating a view model for mensagem ->  MensagemView.js

---
+++ Book project/client/app/ui/views/MensagemView.js
@@ -0,0 +1,11 @@
+class MensagemView{
+
+    constructor(seletor){
+        this._elemento = document.querySelector(seletor)
+    }
+
+    template(model){
+
+        return	`<p	class="alert alert-info">${model.texto}</p>`;
+    }
+}
