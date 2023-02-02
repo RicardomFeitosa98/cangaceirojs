a06f8183826d4798b8a0c089591d23cd5d8a5edb
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-02 10:13:41 -0300

    improvment: small shortcut to reduce codelines added, changes  on NegociacaoController and Negociacoes

--- Book project/client/app/domain/negociacao/Negociacoes.js
+++ Book project/client/app/domain/negociacao/Negociacoes.js
@@ -1,8 +1,9 @@
 class Negociacoes {
 
-  constructor() {
+  constructor(armadilha) {
    
  this._negociacoes = [];
+ this._armadilha= armadilha
  Object.freeze(this)
 
   }
@@ -15,6 +16,7 @@ class Negociacoes {
   adiciona(negociacao) {
 
      this._negociacoes.push(negociacao)
+     this._armadilha(this)
 
   }
   paraArray () {
@@ -22,7 +24,10 @@ class Negociacoes {
     return	[].concat(this._negociacoes);
   }
   esvazia(){
+    
     this._negociacoes.length = 0
+    this._armadilha(this)
+
   }
 
 
