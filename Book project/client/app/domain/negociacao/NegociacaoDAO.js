6cc4732228a926f504442e7566a76ca7bb342417
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-07 16:12:27 -0300

    feat: new class NegociacaoDAO

---
+++ Book project/client/app/domain/negociacao/NegociacaoDAO.js
@@ -0,0 +1,19 @@
+class NegociacaoDAO {
+    constructor(connection){
+        this._connection = connection
+
+        this._store = 'negociacoes'
+    }
+
+    adiciona(negociacao) {
+        return new Promise ((resolve,reject) =>{
+
+        })
+    }
+
+    listaTodos(){
+        return new Promise((resolve,reject)=>{
+            
+        })
+    }
+}
