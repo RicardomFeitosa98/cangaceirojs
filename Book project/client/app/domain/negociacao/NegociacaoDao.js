524d300428b7288a08fd3a7ceb45c870d6d70069
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-07 16:37:11 -0300

    fix: renaming class negociacaoDao for better presentation

---
+++ Book project/client/app/domain/negociacao/NegociacaoDao.js
@@ -0,0 +1,29 @@
+class NegociacaoDAO {
+    constructor(connection){
+        this._connection = connection
+
+        this._store = 'negociacoes'
+    }
+
+    adiciona(negociacao) {
+        return new Promise ((resolve,reject) =>{
+        const request=this._connection
+         .transaction([this._store],'readwrite')
+         .objectStore((this._store))
+         .add(negociacao);
+         request.onsuccess = e => resolve();
+         request.onerror = e => {
+            console.log(e.target.error)
+            reject('não foi possivel salvar a negociação')
+         }
+
+
+        })
+    }
+
+    listaTodos(){
+        return new Promise((resolve,reject)=>{
+
+        })
+    }
+}
